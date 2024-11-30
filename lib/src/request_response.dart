ef1f764758a4f3e1035a9645f167ae3a655dae07
apohodun <apohodun@sdvor.com>
2024-11-30 11:46:40 +0500

    Add lazy headers, parsing utilities, and extended error handling
    - Implement lazy evaluation for headers to optimize performance.
    - Add utility methods (`toInt`, `toDouble`, `toBool`) for convenient header parsing.
    - Enhance the `FormatException` with detailed context for easier debugging.
    - Ensure header values remain immutable for better integrity and reliability.

--- lib/src/request_response.dart
+++ lib/src/request_response.dart
@@ -1,23 +1,25 @@
 import 'dart:typed_data';
 
-import 'package:request_builder/src/request_context.dart';
+import 'package:meta/meta.dart';
 
+import 'request_context.dart';
 import 'response_header.dart';
 
 typedef Bytes = List<int>;
 
+@immutable
 final class RequestResponse {
   final RequestContext request;
   final int statusCode;
   final Uint8List bytes;
-  final List<ResponseHeader> headers;
+  final _HeaderWrapper _headerWrapper;
 
-  const RequestResponse({
+  RequestResponse({
     required this.request,
     required this.statusCode,
     required this.bytes,
-    required this.headers,
-  });
+    required List<ResponseHeader> headers,
+  }) : _headerWrapper = _HeaderWrapper(headers);
 
   RequestResponse copyWith({List<ResponseHeader>? headers}) {
     return RequestResponse(
@@ -28,10 +30,32 @@ final class RequestResponse {
     );
   }
 
+  List<ResponseHeader> get headers => _headerWrapper.headers;
+  ResponseHeader? header(String name) => _headerWrapper.header(name);
+
   /// If request is successfully done
   ///
   /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-  bool get isSuccess => statusCode >= 200 && statusCode < 300;
+  bool get isSuccess => _isStatusInRange(200, 299);
+  bool get isRedirect => _isStatusInRange(300, 399);
+  bool get isClientError => _isStatusInRange(400, 499);
+  bool get isServerError => _isStatusInRange(500, 599);
+
+  bool _isStatusInRange(int start, int end) =>
+      statusCode >= start && statusCode <= end;
 
   bool get isFail => !isSuccess;
+}
+
+final class _HeaderWrapper {
+  final List<ResponseHeader> headers;
+  _HeaderWrapper(this.headers);
+
+  late final Map<String, ResponseHeader> _headers = {
+    for (final header in headers) header.name.toLowerCase(): header,
+  };
+
+  ResponseHeader? header(String name) {
+    return _headers[name.toLowerCase()];
+  }
 }
--- lib/src/response_header.dart
+++ lib/src/response_header.dart
@@ -1,14 +1,63 @@
+import 'dart:collection';
+
 final class ResponseHeader {
   late final String _name;
-  final List<String> values;
+  final List<String> _values;
 
-  ResponseHeader({required String name, required this.values})
-      : _name = name.toLowerCase();
+  ResponseHeader({
+    required String name,
+    required List<String> values,
+  })  : _name = name.toLowerCase(),
+        _values = List.unmodifiable(values);
 
-  bool get isMultiple => values.length > 1;
+  late final String _value = isMultiple ? _values.join(', ') : _values.first;
 
-  String get value => isMultiple ? values.join(', ') : values.first;
+  bool get isMultiple => _values.length > 1;
+  String get value => _value;
   String get name => _name;
+
+  UnmodifiableListView<String> get values => UnmodifiableListView(_values);
+
+  int toInt() {
+    final result = int.tryParse(value);
+    if (result == null) {
+      throw FormatException(
+        'Failed to parse header "$name" with value "$value" as int.',
+      );
+    }
+    return result;
+  }
+
+  double toDouble() {
+    final result = double.tryParse(value);
+    if (result == null) {
+      throw FormatException(
+        'Failed to parse header "$name" with value "$value" as double.',
+      );
+    }
+    return result;
+  }
+
+  bool toBool() {
+    final lowerValue = value.toLowerCase();
+    if (lowerValue == 'true') return true;
+    if (lowerValue == 'false') return false;
+
+    throw FormatException(
+      'Failed to parse header "$name" with value "$value" as bool.',
+    );
+  }
+
+  @override
+  int get hashCode => Object.hash(_name, _values);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ResponseHeader &&
+          runtimeType == other.runtimeType &&
+          _name == other._name &&
+          _values == other._values;
 
   @override
   String toString() => '(name=$name value=$value)';
